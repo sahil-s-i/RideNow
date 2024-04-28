4020ff077a63a494edafa3f18fc514f3632036a0
Sahil <sahilismailnavar@gmail.com>
2024-04-28 20:22:34 +0530

    Added modified jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+// import Navbar from "../src/components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
 
-const App =  () => {
+const App = () => {
   return (
     <div>
-
+      <Navbar />
     </div>
   )
 };
