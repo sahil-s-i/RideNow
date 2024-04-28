5270705889e5dc31eae9609b50bc431e68124a6c
Sahil <sahilismailnavar@gmail.com>
2024-04-28 20:28:28 +0530

    Added navbar items in the file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Navbar =  () => {
+const Navbar = () => {
   return (
-    <div>
-
-    </div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   )
 };
 
