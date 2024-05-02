a9b5acc6c38db287842c3a84577d834dc822287b
Sahil <sahilismailnavar@gmail.com>
2024-05-02 10:16:08 +0530

    Added dark mode and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import Navbar from "../src/components/Navbar/Navbar"
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
+
+  
+  const [theme, setTheme] = React.useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
   return (
     <div>
-      <Navbar />
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 };
