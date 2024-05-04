e1f7117d5644dd78d95da6750c894be7f6140fad
Sahil <sahilismailnavar@gmail.com>
2024-05-04 11:33:16 +0530

    Added grid class and specific divs

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Hero = () => {
-    return <div>Hero</div>
+    return <div className="dark:bg-black dark:text-white duration-300">
+        <div className="container min-h-[620px]">
+            <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Hero;
