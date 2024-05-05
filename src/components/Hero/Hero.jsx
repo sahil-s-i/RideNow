70d0da3441356516a5b2905eb2f0795051edf902
Sahil <sahilismailnavar@gmail.com>
2024-05-05 18:43:43 +0530

    Added car image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,10 @@ const Hero = () => {
     return <div className="dark:bg-black dark:text-white duration-300">
         <div className="container min-h-[620px]">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
-                <div></div>
-                <div></div>
+                <div>
+                    <img src={carpng} alt="" />
+                </div>
+                
             </div>
         </div>
     </div>;
