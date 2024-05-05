55c6a7069447fbdaf309e1e0a6ff8cad5a13e234
Sahil <sahilismailnavar@gmail.com>
2024-05-05 18:51:07 +0530

    Added class names in the divs

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Hero = () => {
     return <div className="dark:bg-black dark:text-white duration-300">
-        <div className="container min-h-[620px]">
+        <div className="container min-h-[620px] flex">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
-                <div>
+                <div className="order-1 sm:order-2">
                     <img src={carpng} alt="" />
                 </div>
-                <div>
-                    <p>Effortless</p>
-                    <h1>Car Rental</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, veniam eum reprehenderit accusantium nihil quibusdam incidunt assumenda dolorum!</p>
+                <div className="order-2 sm:order-1">
+                    <p className="text-primary text-2xl font-serif">Effortless</p>
+                    <h1 className="text-5xl lg:text-7xl font-semibold font-serif">Car Rental</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 </div>
             </div>
         </div>
