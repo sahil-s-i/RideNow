570bb4fa61aee325e330bc41362e7a883d164dcf
Sahil <sahilismailnavar@gmail.com>
2024-05-06 14:51:18 +0530

    Added classes to the div and image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import carpng from "../../assets/car.png"
+import carpng from "../../assets/car.jpeg";
 
 const Hero = () => {
-    return <div className="dark:bg-black dark:text-white duration-300">
+    return <div className="dark:bg-black dark:text-white duration-300 ">
         <div className="container min-h-[620px] flex">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
                 <div className="order-1 sm:order-2">
-                    <img src={carpng} alt="" />
+                    <img src={carpng} alt="" className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
                 </div>
-                <div className="order-2 sm:order-1">
+                <div className="order-2 sm:order-1  space-y-5 sm:pr-32">
                     <p className="text-primary text-2xl font-serif">Effortless</p>
                     <h1 className="text-5xl lg:text-7xl font-semibold font-serif">Car Rental</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
