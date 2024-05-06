66ebbedbee0afeb1ec59755365858b46d12d3946
Sahil <sahilismailnavar@gmail.com>
2024-05-06 15:00:02 +0530

    Added car image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="container min-h-[620px] flex">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
                 <div className="order-1 sm:order-2">
-                    <img src={carpng} alt="" className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
+                    <img src={carpng} alt="Car image" className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
                 </div>
                 <div className="order-2 sm:order-1  space-y-5 sm:pr-32">
                     <p className="text-primary text-2xl font-serif">Effortless</p>
