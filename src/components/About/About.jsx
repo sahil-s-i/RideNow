0496b44bd48f727f11c4446a51a93539d345e4c0
Sahil <sahilismailnavar@gmail.com>
2024-05-07 17:21:50 +0530

    Added About Section folder

---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="grid grid-cols-1 sm:grid-cols-2 place-item-center">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
