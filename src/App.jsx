a0670eacbc9a047090d7e820501433b0af48d14a
Sahil <sahilismailnavar@gmail.com>
2024-05-07 17:21:17 +0530

    Added About section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import About from "./components/About/About";
 
 const App = () => {
 
@@ -36,6 +37,7 @@ const App = () => {
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme} />
+      <About />
     </div>
   )
 };
