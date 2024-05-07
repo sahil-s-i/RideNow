72f67558c25fc41969c6d7daf8c9dad57bad568d
Sahil <sahilismailnavar@gmail.com>
2024-05-07 17:03:42 +0530

    Added zoom in animation with the classes in hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({theme}) => {
     return <div className="dark:bg-black dark:text-white duration-300 relative -z-20">
         <div className="container min-h-[620px] flex">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
-                <div className="order-1 sm:order-2">
+                <div data-aos="zoom-in" data-aos-duration="1500" className="order-1 sm:order-2">
                     <img src={theme === "dark" ? carPng : yellowCarPng} alt="Car image" className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
                 </div>
                 <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
