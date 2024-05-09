c6bc52f4cce48b11a17fb28ea62df9a2fdf0da3c
Sahil <sahilismailnavar@gmail.com>
2024-05-09 19:57:41 +0530

    Added classnames to the div

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,14 +3,16 @@ import CarPng from "../../assets/car1.png";
 
 const About = () => {
     return (
-        <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px]">
+        <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
             <div className="container ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
                     <div>
-                        <img src={CarPng} alt="car png image" />
+                        <img src={CarPng} alt="car png image" className="sm:scale-105 sm:-translate-x-11 max-h-[300px] drop-shadow-[]" />
                     </div>
-                    <div>
+                    <div className="space-y-5 sm:p-16 pb-6 ">
                         <h1 className="text-3xl sm:text-4xl font-bold font-serif">About us</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsam commodi neque vero quo!</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, animi!</p>
                     </div>
                 </div>
             </div>
