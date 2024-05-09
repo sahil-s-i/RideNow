20ef4e7aeab8cb0f93842a63a57b30e50c3fed4b
Sahil <sahilismailnavar@gmail.com>
2024-05-09 20:09:16 +0530

    Added button in the div

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,12 +7,13 @@ const About = () => {
             <div className="container ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
                     <div>
-                        <img src={CarPng} alt="car png image" className="sm:scale-105 sm:-translate-x-11 max-h-[300px] drop-shadow-[]" />
+                        <img src={CarPng} alt="car png image" className="sm:scale-105 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]" />
                     </div>
                     <div className="space-y-5 sm:p-16 pb-6 ">
                         <h1 className="text-3xl sm:text-4xl font-bold font-serif">About us</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsam commodi neque vero quo!</p>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, animi!</p>
+                        <button className="button-outline">Get Started</button>
                     </div>
                 </div>
             </div>
