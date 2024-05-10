e5665ad9088cde921870c2641d6889fd3e611f80
Sahil <sahilismailnavar@gmail.com>
2024-05-10 12:19:24 +0530

    Added sliding effect in the about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
             <div className="container ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-                    <div>
+                    <div data-aos="slide-right" data-aos-duration="1500">
                         <img src={CarPng} alt="car png image" className="sm:scale-105 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]" />
                     </div>
                     <div className="space-y-5 sm:p-16 pb-6">
