e578234e744337990b728fbb77c94467ecfaf9c1
Sahil <sahilismailnavar@gmail.com>
2024-05-10 12:28:01 +0530

    Added slate bg color in the about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import CarPng from "../../assets/car1.png";
 
 const About = () => {
     return (
-        <div className="dark:bg-dark dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
+        <div className="dark:bg-dark bg-slate-100 dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
             <div className="container ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
                     <div data-aos="slide-right" data-aos-duration="1500">
