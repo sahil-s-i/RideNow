447d2453ac937e8166bef57d345fb954ed6442c4
Sahil <sahilismailnavar@gmail.com>
2024-05-10 12:51:39 +0530

    Added code in services file

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Services = () => {
-  return (
-    <div>
-      Services
-    </div>
-  )
+    return (
+        <div className="">
+            <div className="container">
+                <div className="pb-12">
+                    <h1>Why Choose Us</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Services;
