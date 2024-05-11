89ba97083084b101de0081946be11d5bacabb329
Sahil <sahilismailnavar@gmail.com>
2024-05-11 20:11:30 +0530

    Added bootstrap classes in the service section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div className="py-14">
+        <div className="py-14 dark:bg-black dark:text-white sm:min-h[600px] sm:grid sm:place-items-center">
             <div className="container">
                 <div className="pb-12">
                     <h1 className="text-3xl font-semibold text-center font-serif sm:text-4xl">Why Choose Us</h1>
