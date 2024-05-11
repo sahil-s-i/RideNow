a4ba0a46097d54cd2dd3b5bc3253d6b4a2c05048
Sahil <sahilismailnavar@gmail.com>
2024-05-11 20:24:53 +0530

    Added different card elements

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,38 @@
 import React from 'react';
+import { FaCameraRetro } from 'react-icons/fa';
+import { GiNotebook } from 'react-icons/gi';
+import { SlNote } from 'react-icons/sl';
+
+
+const skillsData = [
+    {
+        name: "Best Price",
+        icon: (
+            <FaCameraRetro className="text-5xl text-primary group-hover:text-black duration-300" />
+        ),
+        link: "#",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, magnam?",
+        aosDisplay: "0",
+    },
+    {
+        name: "Fast and Safe",
+        icon: (
+            <GiNotebook className="text-5xl text-primary group-hover:text-black duration-300" />
+        ),
+        link: "#",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, magnam?",
+        aosDisplay: "500",
+    },
+    {
+        name: "Experience Drivers",
+        icon: (
+            <SlNote className="text-5xl text-primary group-hover:text-black duration-500" />
+        ),
+        link: "#",
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, magnam?",
+        aosDisplay: "1000",
+    },
+]
 
 const Services = () => {
     return (
@@ -6,6 +40,13 @@ const Services = () => {
             <div className="container">
                 <div className="pb-12">
                     <h1 className="text-3xl font-semibold text-center font-serif sm:text-4xl">Why Choose Us</h1>
+                </div>
+                <div className="grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {
+                        skillsData.map((skill) => (
+
+                        ))
+                    }
                 </div>
             </div>
         </div>
