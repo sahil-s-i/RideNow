1b8b2bd038d90696ad1b762667d514288783621b
Sahil <sahilismailnavar@gmail.com>
2024-05-12 16:43:31 +0530

    Added title and description

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const CarList = () => {
-  return (
-    <div>    </div>
-  )
+    return (
+        <div className='pb-24'>
+            <div className="container">
+                {/* heading  */}
+                <h1>Lorem, ipsum dolor.</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, maxime.</p>
+            </div>
+        </div>
+    )
 }
 
 export default CarList
