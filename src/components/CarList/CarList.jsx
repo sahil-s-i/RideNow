dac4380fc5142688cc4b519e05d12655aba53f59
Sahil <sahilismailnavar@gmail.com>
2024-05-12 16:58:47 +0530

    Added car list cards div

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react"
 
 const CarList = () => {
     return (
-        <div className='pb-24'>
+        <div className="pb-24 pt-12 bg-white dark:bg-dark dark:text-white">
             <div className="container">
                 {/* heading  */}
-                <h1 data-aos="fade-up" className='text-3xl sm:text-4xl font-semibold font-serif mb-3'>Lorem, ipsum dolor.</h1>
-                <p data-aos="fade-up" className='text-sm pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, maxime.</p>
+                <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-semibold font-serif mb-3">Lorem, ipsum dolor.</h1>
+                <p data-aos="fade-up" className="text-sm pb-10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, maxime.</p>
+                {/* Car listing cards  */}
+                <div>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
+                        {
+
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
