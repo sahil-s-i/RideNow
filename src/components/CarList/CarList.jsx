6c83707c4d25b522c09c8df31f5a7e3828052e17
Sahil <sahilismailnavar@gmail.com>
2024-05-13 18:01:41 +0530

    Added Car list array

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,5 +1,26 @@
 import React from "react"
 
+
+const CarListData = [
+    {
+        name: "BMW UX",
+        price: 100,
+        image: whiteCar,
+        aosDelay: "0",
+    },
+    {
+        name: "KIA UX",
+        price: 140,
+        image: car2,
+        aosDelay: "500",
+    },
+    {
+        name: "BMW UX",
+        price: 100,
+        image: car3,
+        aosDelay: "1000",
+    },
+]
 const CarList = () => {
     return (
         <div className="pb-24 pt-12 bg-white dark:bg-dark dark:text-white">
@@ -11,8 +32,8 @@ const CarList = () => {
                 <div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
                         {
-                            CarList.map((car) => {
-
+                            CarListData.map((car) => {
+                                
                             })
                         }
                     </div>
