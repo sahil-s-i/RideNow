fc8c8fe1727caed5acd7f333b7008a7f898bb1a5
Sahil <sahilismailnavar@gmail.com>
2024-05-15 16:48:56 +0530

    Added heading and description of the testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Testimonial() {
     return (
-        <div>
-            Testimonial
+        <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
+            <div className="container">
+                {/* header  */}
+                <div>
+                    <p>What Our Clients Say About Us</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
         </div>
     )
 }
