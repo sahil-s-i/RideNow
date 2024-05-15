518e46f08aa940e98f1bfbd92ff89f93e6843ca9
Sahil <sahilismailnavar@gmail.com>
2024-05-15 16:52:51 +0530

    Added class names for the div in testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -5,8 +5,8 @@ function Testimonial() {
         <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
             <div className="container">
                 {/* header  */}
-                <div>
-                    <p>What Our Clients Say About Us</p>
+                <div className="space-y-4 pb-12">
+                    <p className="text-3xl font-semibold text-center sm:text-4xl font-serif">What Our Clients Say About Us</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
                 </div>
             </div>
