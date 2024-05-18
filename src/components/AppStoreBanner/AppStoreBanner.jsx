8173b53e3a4e3d19c4c2d294ae5f2c44eaba695a
Sahil <sahilismailnavar@gmail.com>
2024-05-18 19:50:01 +0530

    Added boottrap classes to the div

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AppStoreBanner = () => {
   return (
     <div className="container ">
-      <div className="text-black py-10 sm:min-h-[400px]">
+      <div className="text-black py-10 sm:min-h-[400px] sm:grid sm:place-items-center">
 
       </div>
       
