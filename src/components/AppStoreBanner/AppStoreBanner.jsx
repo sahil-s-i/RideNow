d60e2f704082a621d3779536e45695d4f9ae5193
Sahil <sahilismailnavar@gmail.com>
2024-05-18 20:01:06 +0530

    Added bannerimg array

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+const bannerImg = [
+  background : `url(${pattern})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize : "cover",
+  backgroundPosition : "center",
+
+];
 
 const AppStoreBanner = () => {
   return (
     <div className="container ">
       <div className="text-black py-10 sm:min-h-[400px] sm:grid sm:place-items-center rounded-xl "
-      style={bannerImg}
+        style={bannerImg}
       >
         <div></div>
       </div>
