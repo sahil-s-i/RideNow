54ceee0c8e7b8700df85a3cd658c99db5b3408c5
Sahil <sahilismailnavar@gmail.com>
2024-05-19 12:57:04 +0530

    Added bootstrap classes and image of playstore

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import pattern from "../../assets/website/pattern.jpeg"
+import PlayStoreImg from "../../assets/website/play_store.png"
 
 const bannerImg = {
   background: `url(${pattern})`,
@@ -17,9 +18,14 @@ const AppStoreBanner = () => {
         style={bannerImg}
       >
         <div>
-          <div>
-            <h1>Get Started with our app</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas consequatur qui impedit!</p>
+          <div className="space-y-6 max-w-xl mx-auto">
+            <h1 data-aos="fade up" className="text-2xl text-center sm:text-4xl font-semibold font-serif">Get Started with our app</h1>
+            <p data-aos="fade up" className="text-center sm:px-20">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas consequatur qui impedit!</p>
+            <div>
+              <a href="#">
+                <img src={PlayStoreImg} alt="Play store image" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
