a3cdaf430d4d38c32a2a84a104c3d793888952ce
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:25:51 +0530

    Added class names for  heading and description in the contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,9 +5,14 @@ const Contact = () => {
         <div className="dark:bg-black dark:text-white py-14">
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
+                    <div className="col-span-2 space-y-3">
+                        <h1 className="text-4xl sm:text-5xl font-bold text-white">Let's collaborate on your upcoming car rental venture</h1>
+                        <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi fugit eveniet temporibus reiciendis.</p>
+                    </div>
                     <div>
-                        <h1>Let's collaborate on your upcoming car rental venture</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi fugit eveniet temporibus reiciendis.</p>
+                        <a href="#" className="inline-block font-semibold py-2 px-6 bg-primary text-white rounded-lg tracking-wider uppercase">
+                            Contact
+                        </a>
                     </div>
                 </div>
             </div>
