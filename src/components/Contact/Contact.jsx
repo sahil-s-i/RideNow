f40629ab95bf61f7d85b4c9294a1871c39ef989d
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:15:28 +0530

    Added heading and description in the contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,10 @@ const Contact = () => {
         <div className="dark:bg-black dark:text-white py-14">
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
-
+                    <div>
+                        <h1>Let's collaborate on your upcoming car rental venture</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi fugit eveniet temporibus reiciendis.</p>
+                    </div>
                 </div>
             </div>
         </div>
