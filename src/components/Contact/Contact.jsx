7e35b6422302e355b0dafb184a921b62b310e893
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:07:22 +0530

    Added contact folder

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="dark:bg-black dark:text-white py-14">
+            
+        </div>
+    )
+}
+
+export default Contact;
