dfa46781d1426dc6734c79b643bd44fd19beeb44
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:08:10 +0530

    Added Contact file in the folder

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="dark:bg-black dark:text-white py-14">
-            
+            <div className="container">
+                <div className="grid">
+
+                </div>
+            </div>
         </div>
     )
 }
