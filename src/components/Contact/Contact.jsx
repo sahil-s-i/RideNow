2862459a94cd9f91e039df9540789c1a47c20c76
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:30:41 +0530

    Added contact button in the contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
                         <h1 className="text-4xl sm:text-5xl font-bold text-white">Let's collaborate on your upcoming car rental venture</h1>
                         <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi fugit eveniet temporibus reiciendis.</p>
                     </div>
-                    <div>
+                    <div className="grid place-items-center ">
                         <a href="#" className="inline-block font-semibold py-2 px-6 bg-primary text-white rounded-lg tracking-wider uppercase hover:bg-primary/80 duration-300">
                             Contact
                         </a>
