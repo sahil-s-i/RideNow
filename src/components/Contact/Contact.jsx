604e7695eeec7c0c3e34af620522158972b40d2e
Sahil <sahilismailnavar@gmail.com>
2024-05-20 22:11:43 +0530

    Added Bootstrap classes in the contact file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className="dark:bg-black dark:text-white py-14">
             <div className="container">
-                <div className="grid">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
 
                 </div>
             </div>
