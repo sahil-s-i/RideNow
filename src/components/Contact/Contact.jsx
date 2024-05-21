60184b65f68256bfcc783a20de94997ebccebf7b
Sahil <sahilismailnavar@gmail.com>
2024-05-21 22:41:45 +0530

    Added contact button in the contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
     return (
-        <div className="dark:bg-black dark:text-white py-14">
+        <div data-aos="zoom in" className="dark:bg-black dark:text-white py-14">
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
                     <div className="col-span-2 space-y-3">
