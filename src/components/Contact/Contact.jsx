a76744ef4c2d7b303597c21f0b4834387e2e5cde
Sahil <sahilismailnavar@gmail.com>
2024-05-21 22:42:21 +0530

    Added div

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,6 +13,7 @@ const Contact = () => {
                         <a href="#" className="inline-block font-semibold py-2 px-6 bg-primary text-white rounded-lg tracking-wider uppercase hover:bg-primary/80 duration-300">
                             Contact
                         </a>
+                        <div></div>
                     </div>
                 </div>
             </div>
