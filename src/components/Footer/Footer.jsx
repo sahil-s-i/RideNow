0befcedfca0e4c4ddcc253a582f48ea8fb6e1bd8
Sahil <sahilismailnavar@gmail.com>
2024-05-22 21:26:45 +0530

    Added bootstrap class names in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
         <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
             <div className="container">
                 <div className="grid grid-cols-3 py-5">
-
+                    {/* company details  */}
+                    <div className="py-8 px-4">
+                        <h1>Car Rental</h1>
+                    </div>
                 </div>
             </div>
         </div>
