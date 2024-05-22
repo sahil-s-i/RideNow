969ff57f1b60a8599aa91883316c73cf19968448
Sahil <sahilismailnavar@gmail.com>
2024-05-22 21:24:08 +0530

    Added bootstrap class names in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
+        <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
             <div className="container">
+                <div className="grid grid-cols-3 py-5">
 
+                </div>
             </div>
         </div>
     )
