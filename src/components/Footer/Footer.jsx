ff301e8e1540615cd0db75581590e147f61e80e5
Sahil <sahilismailnavar@gmail.com>
2024-05-22 21:21:02 +0530

    Added container div in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+
+            </div>
+        </div>
+    )
 }
 
 export default Footer
