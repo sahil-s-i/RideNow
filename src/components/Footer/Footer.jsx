173d92f563cf2c8358511a2ad5383c0ddea0dc59
Sahil <sahilismailnavar@gmail.com>
2024-05-22 21:29:42 +0530

    Added bootstrap class names in the heading and description in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,9 @@ const Footer = () => {
                 <div className="grid grid-cols-3 py-5">
                     {/* company details  */}
                     <div className="py-8 px-4">
-                        <h1>Car Rental</h1>
+                        <h1 className="text-xl sm:text-3xl font-bold sm:text-left text-justify mb-3">Car Rental</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt corrupti hic dolorum ex?</p>
+                        <br />
                     </div>
                 </div>
             </div>
