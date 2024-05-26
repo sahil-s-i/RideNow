3556f5ab55115ec9dd4d0a9dd078141db7666ff1
Sahil <sahilismailnavar@gmail.com>
2024-05-26 20:29:43 +0530

    Added bootstrap classes in the code for the div

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import carPng from "../../assets/car.png";
 import yellowCarPng from "../../assets/banner-car.png";
 
 const Hero = ({ theme }) => {
-    return <div className="dark:bg-black dark:text-white duration-300 relative -z-20">
+    return <div className="dark:bg-black dark:text-white duration-300 relative z-20">
         <div className="container min-h-[620px] flex">
             <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 ">
                 <div data-aos="zoom-in" data-aos-duration="1500" className="order-1 sm:order-2">
@@ -12,7 +12,7 @@ const Hero = ({ theme }) => {
                 <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
                     <p data-aos="fade-up" className="text-primary text-2xl font-serif">Effortless</p>
                     <h1 data-aos="fade-up" data-aos-duration="600" className="text-5xl lg:text-7xl font-semibold font-serif">Car Rental</h1>
-                    <p data-aos="fade-up" data-aos-duration="1000">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p data-aos="fade-up" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis dicta officia similique est tenetur quos nostrum quae.</p>
                     <button data-aos="fade-up" data-aos-duration="1500" className="btn bg-primary text-black px-6 py-2 rounded-md duration-300 hover:bg-primary/80 ">Get Started</button>
                 </div>
             </div>
