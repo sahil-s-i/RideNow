d0b6c4feed452b1c29fde8ec144fc759a355d8ad
Sahil <sahilismailnavar@gmail.com>
2024-06-01 22:42:24 +0530

    Added footer in the responsive file

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -6,7 +6,7 @@ const ResponsiveMenu = ({
   showMenu
 }) => {
   return (
-    <div className={`${showMenu ? "left-0" : "-left-full"} fixed top-0 z-50 bg-white dark:bg-gray-900 h-screen w-[75%] md:hidden rounded-r-xl shadow-md flex-col justify-between px-8 pb-6 pt-16 trasition-all duration-300`}>
+    <div className={`${showMenu ? "left-0" : "-left-full"} fixed top-0 z-50 bg-white dark:bg-gray-900 h-screen w-[75%] md:hidden rounded-r-xl shadow-md flex flex-col justify-between px-8 pb-6 pt-16 trasition-all duration-300`}>
       <div className="card">
         <div className="flex items-center justify-start gap-3">
           <FaUserCircle size={50} />
@@ -29,7 +29,9 @@ const ResponsiveMenu = ({
         </nav>
       </div>
       {/* Footer  */}
-      <div></div>
+      <div>
+        <h1>Footer content</h1>
+      </div>
     </div>
   )
 }
