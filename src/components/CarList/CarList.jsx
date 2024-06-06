bbb6c85b2c002ea8b319234026367aad67cae55c
Sahil <sahilismailnavar@gmail.com>
2024-06-06 22:21:18 +0530

    Changed the heading name and description

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -29,8 +29,8 @@ const CarList = () => {
         <div className="pb-24 pt-12 bg-white dark:bg-dark dark:text-white">
             <div className="container">
                 {/* heading  */}
-                <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-semibold font-serif mb-3">Lorem, ipsum dolor.</h1>
-                <p data-aos="fade-up" className="text-sm pb-10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, maxime.</p>
+                <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-semibold font-serif mb-3">Our Premium Car Selection</h1>
+                <p data-aos="fade-up" className="text-sm pb-10">Explore our diverse range of high-quality vehicles designed to provide you with the ultimate driving experience. Whether for business or leisure, we have the perfect car for you.</p>
                 {/* Car listing cards  */}
                 <div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
