2edcba3ebd9a3fcee9aae8e86674322aea8f3ca0
Sahil <sahilismailnavar@gmail.com>
2024-06-08 21:36:29 +0530

    Added person1 image in the code

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import person1 from "../../assets/Testimonial-images/person1.jpg";
 
 const TestimonialsData = [
     {
         name: "Dilshad",
-        image: "",
+        image : person1,
         description: "The rental process was smooth and hassle-free. The car was in excellent condition and the customer service was top-notch.",
         aosDelay: "0",
     },
     {
         name: "Satya",
-        image: "",
+        image : "",
         description: "I had a fantastic experience with this car rental service. The staff was friendly and helpful, and the prices were very reasonable.",
         aosDelay: "300",
     },
     {
         name: "Sabir",
-        image: "",
+        image : "",
         description: "Excellent service and a great selection of vehicles. I highly recommend them for anyone in need of a reliable rental car.",
         aosDelay: "1000",
     },
