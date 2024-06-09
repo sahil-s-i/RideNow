617840b7fe754a3dec038d2089f225c6ba3ec46a
Sahil <sahilismailnavar@gmail.com>
2024-06-09 22:19:31 +0530

    Modified the code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        
         <div data-aos="zoom in" className="dark:bg-black dark:text-white py-14">
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
