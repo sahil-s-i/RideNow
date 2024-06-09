59ed3a9863ac0d79b2ee807ed8e7b856d2446026
Sahil <sahilismailnavar@gmail.com>
2024-06-09 22:16:52 +0530

    Added description content in the code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,8 @@ const Contact = () => {
     return (
         <div data-aos="zoom in" className="dark:bg-black dark:text-white py-14">
             <div className="container">
-                <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6
+                ">
                     <div className="col-span-2 space-y-3">
                         <h1 className="text-4xl sm:text-5xl font-bold text-white">Let's collaborate on your upcoming car rental venture</h1>
                         <p className="text-gray-400">Join us to make your car rental experience seamless and enjoyable. We offer unparalleled service and a wide selection of vehicles to meet all your needs.</p>
