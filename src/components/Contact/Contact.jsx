509f243f2cf53bc2faef364707be80fca007a432
Sahil <sahilismailnavar@gmail.com>
2024-06-10 22:43:06 +0530

    Modified the code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,6 @@ const Contact = () => {
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
                     <div className="col-span-2 space-y-3">
                         <h1 className="text-4xl sm:text-5xl font-bold text-white">Let's collaborate on your upcoming car rental venture</h1>
-                        
                         <p className="text-gray-400">Join us to make your car rental experience seamless and enjoyable. We offer unparalleled service and a wide selection of vehicles to meet all your needs.</p>
                     </div>
                     <div className="grid place-items-center ">
