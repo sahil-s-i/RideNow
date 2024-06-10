7954a7e12309c22f4da660c9af42b63b4d1f11a4
Sahil <sahilismailnavar@gmail.com>
2024-06-10 22:42:36 +0530

    Modified the code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,7 +12,6 @@ const Contact = () => {
                     <div className="grid place-items-center ">
                         <a href="#" className="inline-block font-semibold py-2 px-6 bg-primary text-white rounded-lg tracking-wider uppercase hover:bg-primary/80 duration-300">
                             Contact
-                            
                         </a>
                     </div>
                 </div>
